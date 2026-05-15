d2e074b2a5cc49bafdcb985a7632eb7bad045a11
BluestoneMario <lennarthellwig@yahoo.de>
2026-05-15 09:42:14 +0200

    Move Export/Import to History tab, add calendar view

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * and clients pick up new assets on next activate.
  */
 
-const APP_VERSION = '1.0.0';
+const APP_VERSION = '1.0.3';
 const STATIC_CACHE = `workouttracker-static-${APP_VERSION}`;
 const FONTS_CACHE  = `workouttracker-fonts-${APP_VERSION}`;
 
