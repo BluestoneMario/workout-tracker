401ae81911fd75a4e578eb2d77a3e556e41dc70c
BluestoneMario <lennarthellwig@yahoo.de>
2026-05-15 23:38:20 +0200

    Refactor nav to two-tab layout, move run logger to bottom sheet, bump sw v1.0.8

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * and clients pick up new assets on next activate.
  */
 
-const APP_VERSION = '1.0.5';
+const APP_VERSION = '1.0.8';
 const STATIC_CACHE = `workouttracker-static-${APP_VERSION}`;
 const FONTS_CACHE  = `workouttracker-fonts-${APP_VERSION}`;
 
